502830265264846ee0aa69d361c8a425704c5cc0
unknown <davis@davisarchibald.com>
2013-02-27 08:14:39 -0700

    add event listeners for hover, assign variable names to layers

--- js/map.js
+++ js/map.js
@@ -1,53 +1,94 @@
+/* Map */
 var map = L.map('recyclingMap').setView([40.298753, -111.696486], 13);
+/* Map Tile Layer*/
 L.tileLayer('http://{s}.tile.cloudmade.com/ef14c18343234773af86eb0bc3882836/997/256/{z}/{x}/{y}.png', {
 	maxZoom: 18,
-	minZoom : 13
+	minZoom : 12
 }).addTo(map);
-/* L.geoJson(oremData, {
-	style : myStyle
-}).addTo(map);*/
-L.geoJson(recyclingone, {
-	style : {
-		fillColor: "blue",
-	color: "blue",
-	weight: 1,
-	opacity:0,
-	fillOpacity:0.4
-	}
-}).addTo(map);
-L.geoJson(recyclingtwo, {
-	style : {
-		fillColor: "green",
-	color: "green",
-	weight: 1,
-	opacity:0,
-	fillOpacity:0.4
-	}
-}).addTo(map);
-L.geoJson(recyclingthree, {
-	style : {
-		fillColor: "orange",
-	color: "orange",
-	weight: 1,
-	opacity:0,
-	fillOpacity:0.4
-	}
-}).addTo(map);
-L.geoJson(recyclingfour, {
-	style : {
-		fillColor: "yellow",
-	color: "yellow",
-	weight: 1,
-	opacity:0,
-	fillOpacity:0.4
-	}
-}).addTo(map);
-L.geoJson(recyclingfive, {
+/* Recycling Region Layers*/
+var monday = L.geoJson(recyclingfive, {
 	style : {
 		fillColor: "red",
-	color: "red",
-	weight: 1,
-	opacity:0,
-	fillOpacity:0.4
+		color: "red",
+		weight: 1,
+		opacity:0,
+		fillOpacity:0.4
 	}
-}).addTo(map);
+}).addTo(map).addEventListener('mouseover', function () {
+	this.setStyle({
+		fillOpacity : 0.6
+	});
+}).addEventListener('mouseout', function () {
+	this.setStyle({
+		fillOpacity : 0.4
+	});
+}),
+tuesday = L.geoJson(recyclingtwo, {
+	style : {
+		fillColor: "green",
+		color: "green",
+		weight: 1,
+		opacity:0,
+		fillOpacity:0.4
+	}
+}).addTo(map).addEventListener('mouseover', function () {
+	this.setStyle({
+		fillOpacity : 0.6
+	});
+}).addEventListener('mouseout', function () {
+	this.setStyle({
+		fillOpacity : 0.4
+	});
+}),
+wednesday = L.geoJson(recyclingone, {
+	style : {
+		fillColor: "blue",
+		color: "blue",
+		weight: 1,
+		opacity:0,
+		fillOpacity:0.4
+	}
+}).addTo(map).addEventListener('mouseover', function () {
+	this.setStyle({
+		fillOpacity : 0.6
+	});
+}).addEventListener('mouseout', function () {
+	this.setStyle({
+		fillOpacity : 0.4
+	});
+}),
+thursday = L.geoJson(recyclingthree, {
+	style : {
+		fillColor: "orange",
+		color: "orange",
+		weight: 1,
+		opacity:0,
+		fillOpacity:0.4
+	}
+}).addTo(map).addEventListener('mouseover', function () {
+	this.setStyle({
+		fillOpacity : 0.6
+	});
+}).addEventListener('mouseout', function () {
+	this.setStyle({
+		fillOpacity : 0.4
+	});
+}),
+friday = L.geoJson(recyclingfour, {
+	style : {
+		fillColor: "yellow",
+		color: "yellow",
+		weight: 1,
+		opacity:0,
+		fillOpacity:0.4
+	}
+}).addTo(map).addEventListener('mouseover', function () {
+	this.setStyle({
+		fillOpacity : 0.6
+	});
+}).addEventListener('mouseout', function () {
+	this.setStyle({
+		fillOpacity : 0.4
+	});
+});
+
